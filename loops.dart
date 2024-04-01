998489e1ab0821e9aeaaa52f1fe7025f79ff3e57
Markayo21 <159958226+Markayo21@users.noreply.github.com>
2024-04-01 22:38:18 +0300

    Update loops.dart

--- loops.dart
+++ loops.dart
@@ -1,10 +1,10 @@
 void main (){
   var number= 14;
-  if (number > 10);{
+  if (number > 10){
    print("Your number is greater than 10");
-  } if(number < 10);{
+  } else if(number < 10){
     print("Your number is less than 10");
-  } if (number == 10);{
+  } else if (number == 10){
    print("Your number is equal to 10");}
 
 }
